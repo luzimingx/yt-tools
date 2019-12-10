0074a658ebc5125536d7eaeae74fe64df6f0fde1
luziming <luzimingx@gmail.com>
2019-12-10 11:05:50 +0800

    增加拖动功能

--- content/index.js
+++ content/index.js
@@ -26,7 +26,7 @@ var Tool = {
         var _this = this;
         if (!this.isYitang()) return false;
         this.changeToHttps();
-        chrome.storage.sync.get(['developerName'], function(result) {
+        chrome.storage.sync.get(['developerName', 'position'], function(result) {
             if (result.developerName) {
                 _this.modes.push({
                     name: '开发',
@@ -34,7 +34,7 @@ var Tool = {
                     class: 'btn-default'
                 });
             }
-            _this.addTool();
+            _this.addTool(result);
         });
     },
     isYitang: function() {
@@ -44,9 +44,12 @@ var Tool = {
         if (location.protocol.slice(0, -1) === 'https') return true;
         location.href = location.href.replace(/^.*(\:\/\/.+$)/, 'https$1');
     },
-    addTool: function() {
-        var _this = this;
-        var developTol = this.createEl('div', { style: 'position: fixed;z-index: 99;bottom: 2rem;left: 0;border: 1px dotted #eee' });
+    addTool: function(data) {
+        var _this = this,
+            position = data.position ? JSON.parse(data.position) : { x: 0, y: 0 };
+        var developTol = this.createEl('div', { style: 'position: fixed;z-index: 99;border: 1px dotted #eee' });
+        developTol.style.left = position.x + 'px';
+        developTol.style.top = position.y + 'px';
         // 切换模式
         var modeBtn = this.createEl('button', { class: "btn btn-sm btn-block" });
         this.getCurrentMode();
@@ -61,7 +64,51 @@ var Tool = {
             linkEl.innerHTML = link.name;
             developTol.appendChild(linkEl);
         });
-        document.body.appendChild(developTol);
+        this.addListeners(developTol);
+        document.body.parentNode.appendChild(developTol);
+    },
+    addListeners(developTol) {
+        var _this = this,
+            positionStart,
+            positionMax,
+            positionNow = {},
+            mouseStart,
+            moveFlag = false;
+        // 拖动开始
+        developTol.addEventListener('touchstart', function(e) {
+            positionStart = developTol.getBoundingClientRect();
+            positionMax = {
+                x: window.screen.availWidth * 2 - positionStart.width,
+                y: window.screen.availHeight * 2 - positionStart.height,
+            };
+            mouseStart = {
+                x: e.targetTouches[0].pageX,
+                y: e.targetTouches[0].pageY
+            };
+        }, false);
+        // 拖动中
+        developTol.addEventListener('touchmove', function(e) {
+            moveFlag = true;
+            e.preventDefault();
+            positionNow.x = _this.getPosition(0, positionMax.x, positionStart.x + e.targetTouches[0].pageX - mouseStart.x);
+            positionNow.y = _this.getPosition(0, positionMax.y, positionStart.y + e.targetTouches[0].pageY - mouseStart.y);
+            developTol.style.left = positionNow.x + 'px';
+            developTol.style.top = positionNow.y + 'px';
+        }, false);
+        // 拖动结束 存储位置
+        developTol.addEventListener('touchend', function(e) {
+            if (!moveFlag) return;
+            chrome.storage.sync.set({
+                position: JSON.stringify(positionNow)
+            }, function() {
+                moveFlag = false;
+            });
+        }, false);
+    },
+    getPosition(min, max, current) {
+        if (current < min) return min;
+        if (current > max) return max;
+        return current;
     },
     createEl: function(type, options) {
         var el = document.createElement(type);
